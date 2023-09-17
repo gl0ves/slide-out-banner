479735481d2ac1cca433c93cf88705562460b128
gl0ves <32078347+gl0ves@users.noreply.github.com>
2023-09-17 13:44:58 +0300

    Some ypa specific changes

--- src/components/SlidingBanner.vue
+++ src/components/SlidingBanner.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="banner" :class="{ 'slide-in': showBanner }">
-    <img alt="close banner" class="x-icon" src="@/assets/x.svg" @click="closeBanner()">
-      <h3>You're invited to our {{ decodedLocation }} Facebook group!</h3>
-      <p>Stay updated on new <span class="bold">{{ decodedLocation }}</span> properties for sale via our <span class="bold">private Facebook group.</span></p>
-      <a :href="facebookGroupUrl" target="_blank">
-        <button type="button" class="facebook-btn">
-          JOIN FACEBOOK GROUP
-        </button>
-      </a>
+  <div
+    class="banner"
+    :class="{ 'slide-in': showBanner }"
+  >
+    <v-icon
+      alt="close banner"
+      class="x-icon"
+      @click="closeBanner()"
+    >
+      mdi-close
+    </v-icon>
+    <h3>You're invited to our {{ decodedLocation }} Facebook group!</h3>
+    <p>
+      Stay updated on new <span class="bold">{{ decodedLocation }}</span> properties for sale via our <span
+        class="bold"
+      >private Facebook group.</span>
+    </p>
+    <a
+      :href="facebookGroupUrl"
+      target="_blank"
+    >
+      <button
+        type="button"
+        class="facebook-btn"
+      >
+        JOIN FACEBOOK GROUP
+      </button>
+    </a>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script>
 
-export default defineComponent({
+export default {
   name: 'SlidingBanner',
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
   data() {
     return {
       showBanner: false,
@@ -24,8 +48,17 @@ export default defineComponent({
       facebookGroupUrl: ''
     };
   },
+  computed: {
+    locations() {
+      return ["costa-almería", "costa-blanca", "costa-del-sol", "costa-cálida", "gran-canaria", "ibiza", "lanzarote", "mallorca", "menorca", "tenerife"];
+    }
+  },
   mounted() {
+    if(!this.show) return
     const location = this.parseLocationFromURL();
+    if (localStorage.getItem('location')) {
+      return;
+    }
     if (location) {
       this.location = location;
       const capitalizedLocation = this.capitalizedLocation(decodeURIComponent(location));
@@ -33,71 +66,107 @@ export default defineComponent({
       this.facebookGroupUrl = this.generateFacebookGroupUrl(location);
       setTimeout(() => {
         this.showBanner = true;
+        localStorage.setItem('location', location);
       }, 1000);
     }
   },
   methods: {
-    capitalizedLocation(location: string) {
+    capitalizedLocation(location) {
       let words = location.split(' '); // Split location string into array with words separated by spaces
       let capitalizedLocation = words.map((word, index) => { // Map over each word and its index in the array
         if (index === 0 || index === words.length - 1) { // If first (index 0) or last (index equal to the length of array - 1) word in array
-          return word.charAt(0).toUpperCase() + word.slice(1); // Capitalize the character at index 0 of word 
+          return word.charAt(0).toUpperCase() + word.slice(1); // Capitalize the character at index 0 of word
         } else {
           return word.toLowerCase(); // Lowercase for all other words (i.e. "Costa del Sol")
         }
-        }).join(' '); // Join array back into string with spaces
-        return capitalizedLocation;
+      }).join(' '); // Join array back into string with spaces
+      return capitalizedLocation;
+    },
+    getRegion(region) {
+      switch (region) {
+        case 'costa-almería':
+          return "Costa Almería"
+        case 'costa-blanca':
+          return "Costa Blanca"
+        case 'costa-del-sol':
+          return "Costa del Sol"
+        case 'costa-cálida':
+          return 'Costa Calida'
+        case 'gran-canaria':
+          return 'Gran Canaria'
+        case 'ibiza':
+          return 'Ibiza'
+        case 'lanzarote':
+          return 'Lanzarote'
+        case 'mallorca':
+          return 'Mallorca'
+        case 'menorca':
+          return 'Menorca'
+        case "tenerife":
+          return 'Tenerife'
+        default:
+          return null
+      }
     },
     parseLocationFromURL() {
-      const url = window.location.href; // Get URL
-      return url.split('/').pop()?.replace(/-/g, ' '); // Get last part of URL and replace dashes with spaces for readability
+      const url = decodeURIComponent(window.location.href) // Get URL
+      for (let i = 0; i < this.locations.length; i++) {
+        if (url.includes(this.locations[i])) {
+          const location = this.locations[i];
+          return this.getRegion(location)
+        }
+      }
     },
-    generateFacebookGroupUrl(location: string) {
-      const sanitizedLocation = location.replace(/%C3%AD/g, 'i').replace(/%C3%A1/g, 'a').replace(/ /g, ''); // Remove accents and spaces from location string to match Facebook group URL
-      return `https://www.facebook.com/groups/propertiesforsalein${sanitizedLocation}`; // Insert sanitized location into Facebook group URL
+    generateFacebookGroupUrl(location) {
+      return `https://www.facebook.com/groups/propertiesforsalein${location}`; // Insert location into Facebook group URL
     },
     closeBanner() {
-    this.showBanner = false;
+      this.showBanner = false;
+    },
   }
-  }
-});
+};
 </script>
 
 <style scoped>
 .banner {
-  width: 100%;
   border: 1px solid rgb(221, 219, 219);
   background-color: #fff;
   border-radius: 4px;
   display: flex;
   flex-direction: column;
   position: fixed;
-  bottom: -50%; /* Initially hidden off-screen at the bottom */
+  left: 0;
+  bottom: -50%;
+  /* Initially hidden off-screen at the bottom */
   /* top: -50%; IN CASE OF CHAT BOT */
   padding: 1.3rem;
   gap: 20px;
 }
 
 .slide-in {
-  animation: slide-in 2.5s ease-in-out forwards; /* I thought this was a good sliding speed */
+  animation: slide-in 2.5s ease-in-out forwards;
+  /* I thought this was a good sliding speed */
 }
 
 @keyframes slide-in {
   0% {
-    bottom: -100%; /* Start off-screen */
+    bottom: -100%;
+    /* Start off-screen */
   }
+
   100% {
-    bottom: 0rem; /* Slide up the screen to the bottom */
+    bottom: 0rem;
+    /* Slide up the screen to the bottom */
   }
 }
 
 /* IN CASE OF CHAT BOT: /*
 /* @keyframes slide-in {
   0% {
-    top: -100%; 
+    top: -100%;
  }
   100% {
-    top: 0rem; 
+    top: 0rem;
  }
 } */
 
@@ -139,32 +208,36 @@ button {
 
 @media (min-width: 660px) and (max-width: 1023px) {
   button {
-  width: 60%;
-  display: flex;
-  justify-content: center;
-  margin: auto;
-}
+    width: 60%;
+    display: flex;
+    justify-content: center;
+    margin: auto;
+  }
 
-.banner {
-  gap: 15px;
-}
+  .banner {
+    gap: 15px;
+  }
 }
 
 @media (min-width: 1024px) {
   .banner {
-  width: 285px;
-  bottom: 2rem;
-  left: -40%; /* Initially hidden off-screen to the left */
-  gap: 8px;
-}
+    width: 285px;
+    bottom: 2rem;
+    left: -40%;
+    /* Initially hidden off-screen to the left */
+    gap: 8px;
+  }
 
   @keyframes slide-in {
-  0% {
-    left: -100%; /* Start off-screen to the left */
+    0% {
+      left: -100%;
+      /* Start off-screen to the left */
+    }
+
+    100% {
+      left: 2rem;
+      /* Slide in to the current position at the left-hand corner */
+    }
   }
-  100% {
-    left: 2rem; /* Slide in to the current position at the left-hand corner */
-  }
-}
 }
 </style>
